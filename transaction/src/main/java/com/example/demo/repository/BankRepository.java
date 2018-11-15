c8d96ee9f9f077942d91bd62c0ea594ded8505e9
hdmxz430 <junboq@andrew.cmu.edu>
2018-11-15 10:53:09 -0800

    delete controller

--- transaction/src/main/java/com/example/demo/repository/BankRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Bank;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BankRepository extends JpaRepository<Bank, Long>{
-
-}
